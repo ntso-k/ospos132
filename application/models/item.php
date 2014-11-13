1538ef9d670a219deb67aed37bfae125f2c27133
ntso-k <ntso.king@gmail.com>
2014-11-14 00:55:31 +0800

    application\models\item.php

--- application/models/item.php
+++ application/models/item.php
@@ -188,8 +188,6 @@ class Item extends CI_Model
 	{
 		$suggestions = array();
 
-			return $suggestions;
-
 		$this->db->from('items');
 		$this->db->like('name', $search);
 		$this->db->where('deleted',0);
